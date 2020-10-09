b4497b41cf9943284101bc5d4bcd887779a98f9c
danieleMoli <molinari.daniele@iisgalvanimi.edu.it>
2020-10-09 09:25:35 +0000

    esercizio 15.2

--- progetto/src/app/app.module.ts
+++ progetto/src/app/app.module.ts
@@ -1,18 +1,20 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+import { FooComponent } from './foo/foo.component';
 
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   FooComponent
+ ],
+ imports: [
+   BrowserModule, HttpClientModule //Aggiungi il modulo http qui
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
